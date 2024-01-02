20051eb0070cd327ca142e3ffee5767dd96242e9
xuwen <xuwen@juzix.net>
2024-01-02 17:07:11 +0800

    Add interface for stake handler contract module

--- apps/indexer/lib/indexer/fetcher/platon_appchain/contracts/stake_handler.ex
+++ apps/indexer/lib/indexer/fetcher/platon_appchain/contracts/stake_handler.ex
@@ -1,8 +1,11 @@
 defmodule Indexer.Fetcher.PlatonAppchain.Contracts.StakeHandler do
+  @moduledoc """
+  Stake handler contract interface encapsulation
+  """
   alias Ethers
   use Ethers.Contract, abi_file: "config/abi/StakeHandler.json", default_address: Application.get_env(:indexer, Contracts)[:l2_stake_handler]
 
-  def convertValidatorToJSON(validator) do
+  defp convertValidatorToJSON(validator) do
     data = %{
       "validatorAddr" => elem(validator, 0),
       "owner" => elem(validator, 1),
@@ -18,11 +21,206 @@ defmodule Indexer.Fetcher.PlatonAppchain.Contracts.StakeHandler do
     data
   end
 
+  defp convertDelegationToJSON(delegation) do
+    data = %{
+      "validatorAddr" => elem(delegation, 0),
+      "delegatorAddr:" => elem(delegation, 1),
+      "amount" => elem(delegation, 2),
+      "stakeEpoch"=> elem(delegation, 3),
+      "delegateEpoch"=> elem(delegation, 4),
+    }
+    data
+  end
+
+  @doc """
+  Query the list of all validators, Support pagination to query the list of all validators
+
+  ## Parameters
+    * `start`(bytes) - represents the starting query ID. When passing empty bytes, it defaults to starting from the first Id
+    * `size`(integer) - page size
+
+  ## Returns
+    * bytes of next start
+    * ValidatorInfo array for query
+
+  ## Examples
+     {"7072696F7269747956616C696461746F7200000000000000000000000000FFFFFFFFFFFFFFFFFFC46535FF00000000000000010000000000000002",
+  [
+   %{
+     "blsKey" => "828B858EAB99526F901CE610AE3D2D9B08F2302E56A29698776D2135E94FC074C575A53E27B8A68E9A7692F3BE65965A",
+     "commissionRate" => 100,
+     "delegateAmount" => 10000,
+     "epoch" => 1,
+     "owner" => "0x70d207c1322ccb9069d3790d6768866dabff1035",
+     "pubKey" => "8D84E41F83E833F622C45766E7E425CF03A225867FACB05BAF90EAF29C1BF53680988DAB4E6F058759871C91B3E6FF888AABC41DAEC4B8E0877FC8FE8FEED27F",
+     "stakeAmount" => 1000000000,
+     "stakeIndex" => 1,
+     "status" => 0,
+     "validatorAddr" => "0x70d207c1322ccb9069d3790d6768866dabff1035"
+   },
+   %{
+     "blsKey" => "B6F85C577FF890F9737595A9E326D5539CC1CC859C879017CF25ACCEF8A96518EC9747EF621625063DC17AE95300A74F",
+     "commissionRate" => 100,
+     "delegateAmount" => 0,
+     "epoch" => 1,
+     "owner" => "0x70d207c1322ccb9069d3790d6768866dabff1035",
+     "pubKey" => "5C79BF8B836BDC85FE513A64A558291E96AC2405B6B95D5CA05BB20DB9C0A1A00E12D11DD540D8685267015CE71CEF43781A75157EC6F266CC88A8FB8B5C6C17",
+     "stakeAmount" => 1000000000,
+     "stakeIndex" => 0,
+     "status" => 0,
+     "validatorAddr" => "0x1dd26dfb60b996fd5d5152af723949971d9119ee"
+   }
+  ]}
+  """
   def getValidators(start, size) do
     result = get_validators(start, size) |> Ethers.call()
     {:ok, data} = result
     [nextStart | validators] = data
     validatorsJson = List.first(validators) |> Enum.map(fn validator -> convertValidatorToJSON(validator) end)
     {Base.encode16(nextStart), validatorsJson}
+  end
+
+  @doc """
+  /// @notice Query the list of validators by addrs
+    /// @dev Support to query the list of validators by addr of validators
+    /// @param validators addr of validators
+    /// @return ValidatorInfo array for query
+  Query the list of validators by addrs。Support to query the list of validators by addr of validators
+
+  ## Parameters
+    * `validators`(address[]) - addr of validators
+
+  ## Returns
+    * ValidatorInfo array for query
+
+  ## Examples
+    [
+  %{
+    "blsKey" => "828B858EAB99526F901CE610AE3D2D9B08F2302E56A29698776D2135E94FC074C575A53E27B8A68E9A7692F3BE65965A",
+    "commissionRate" => 100,
+    "delegateAmount" => 10000,
+    "epoch" => 1,
+    "owner" => "0x70d207c1322ccb9069d3790d6768866dabff1035",
+    "pubKey" => "8D84E41F83E833F622C45766E7E425CF03A225867FACB05BAF90EAF29C1BF53680988DAB4E6F058759871C91B3E6FF888AABC41DAEC4B8E0877FC8FE8FEED27F",
+    "stakeAmount" => 1000000000,
+    "stakeIndex" => 1,
+    "status" => 0,
+    "validatorAddr" => "0x70d207c1322ccb9069d3790d6768866dabff1035"
+  }
+  ]
+  """
+  def getValidatorsWithAddr(validators) do
+    result = get_validators_with_addr(validators) |> Ethers.call()
+    {:ok, validators} = result
+    validatorsJson = validators |> Enum.map(fn validator -> convertValidatorToJSON(validator) end)
+    validatorsJson
+  end
+
+  @doc """
+  Query the list of validators for a certain period。For the convenience of expanding the list of validators with multiple period properties
+
+  ## Parameters
+    * `periodType`(integer) - represents a period of a certain type
+    * `period`(integer) - represents the number of intervals
+
+  ## Returns
+    * validator address array
+
+  ## Examples
+    ["0x1dd26dfb60b996fd5d5152af723949971d9119ee","0x70d207c1322ccb9069d3790d6768866dabff1035","0x343972bf63d1062761aaaa891d2750f03cb4b2f7"]
+  """
+  def getValidatorAddrs(periodType, period) do
+    result = get_validator_addrs(periodType, period) |> Ethers.call()
+    {:ok, addrs} = result
+    addrs
+  end
+
+  @doc """
+  Query the delegation information of the delegator on these validators based on the addr list of validators
+
+  ## Parameters
+    * `validators`(address[]) - addr of validators
+    * `delegator`(address) - the delegator
+
+  ## Returns
+    * DelegationInfo array for query
+
+  ## Examples
+    [%{
+      "amount" => 10000,
+      "delegateEpoch" => 1395,
+      "delegatorAddr:" => "0x62953f9213f899f2a51680c2fbb4282a2591bfc8",
+      "stakeEpoch" => 1,
+      "validatorAddr" => "0x70d207c1322ccb9069d3790d6768866dabff1035"
+    }]
+  """
+  def getDelegationsWithValidator(validators, delegator) do
+    result =  get_delegations_with_validator(validators, delegator) |> Ethers.call()
+    {:ok, delegations} = result
+    delegationJson = delegations |> Enum.map(fn delegation -> convertDelegationToJSON(delegation) end)
+    delegationJson
+  end
+
+  @doc """
+  Query how much is yet to become withdrawable for account.
+
+  ## Parameters
+    * `validator`(address) - The validator to calculate amount for
+    * `delegator`(address) - The delegator to calculate amount for
+
+  ## Returns
+    * Amount not yet withdrawable
+
+  """
+  def pendingWithdrawalsOfDelegate(validator, delegator) do
+    result = pending_withdrawals_of_delegate(validator, delegator) |> Ethers.call()
+    {:ok, pendingWithdrawals} = result
+    pendingWithdrawals
+  end
+
+  @doc """
+  Query how much is yet to become withdrawable for account.
+
+  ## Parameters
+    * `validator`(address) - The validator to calculate amount for
+
+  ## Returns
+    * Amount not yet withdrawable
+  """
+  def pendingWithdrawalsOfStake(validator) do
+    result = pending_withdrawals_of_stake(validator) |> Ethers.call()
+    {:ok, pendingWithdrawals} = result
+    pendingWithdrawals
+  end
+
+  @doc """
+  Query how much can be withdrawn for account in this epoch.
+
+  ## Parameters
+    * `validator`(address) - The validator to calculate amount for
+    * `delegator`(address) - The delegator to calculate amount for
+
+  ## Returns
+    * Amount withdrawable
+  """
+  def withdrawableOfDelegate(validator, delegator) do
+    result = withdrawable_of_delegate(validator) |> Ethers.call()
+    {:ok, withdrawable} = result
+    withdrawable
+  end
+
+  @doc """
+  Query how much can be withdrawn for account in this epoch.
+
+  ## Parameters
+    * `validator`(address) - The account to calculate amount for
+
+  ## Returns
+    * Amount withdrawable
+  """
+  def withdrawableOfStake(validator) do
+    result = withdrawable_of_stake(validator) |> Ethers.call()
+    {:ok, withdrawable} = result
+    withdrawable
   end
 end
