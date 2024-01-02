92eb61efa9108fbd59174c6fd321b9846fc1d586
xuwen <xuwen@juzix.net>
2024-01-02 19:52:59 +0800

    Add rpc_opts in contract module

---
+++ apps/indexer/lib/indexer/fetcher/platon_appchain/contracts/l1_stake_manager.ex
@@ -0,0 +1,12 @@
+defmodule Indexer.Fetcher.PlatonAppchain.Contracts.StakeManager do
+  alias Ethers
+  use Ethers.Contract, abi_file: "config/abi/L1_StakeManager.json", default_address: Application.get_env(:indexer, Contracts)[:l1_stake_manager]
+
+  @rpc_opts [url: Application.get_all_env(:indexer)[Indexer.Fetcher.PlatonAppchain][:platon_appchain_l1_rpc], http_headers: [{"Content-Type", "application/json"}]]
+
+  def totalStake() do
+    result = StakeManager.total_stake() |> Ethers.call(rpc_opts: @rpc_opts)
+    {:ok, data} = result
+    data
+  end
+end
