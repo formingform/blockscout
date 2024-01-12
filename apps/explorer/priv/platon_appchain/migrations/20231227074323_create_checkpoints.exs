c11d66110bfcf7702deb24386e704d50faede722
formingform <formgingform.lyu@gmail.com>
2024-01-12 22:11:00 +0800

    modify checkpoints table definition

--- apps/explorer/priv/platon_appchain/migrations/20231227074323_create_checkpoints.exs
+++ apps/explorer/priv/platon_appchain/migrations/20231227074323_create_checkpoints.exs
@@ -3,25 +3,24 @@ defmodule Explorer.Repo.PlatonAppchain.Migrations.CreateCheckPoints do
 
   def change do
     create table(:checkpoints, primary_key: false) do
+      # check point 周期
+      add(:epoch, :bigint, null: false, primary_key: true)
+      # check point 周期 对应截止块高
+      add(:block_number, :bigint, null: false)
       # （l2_block_start +l2_block_end）组合id
-      add(:start_end_block, :string, null: false, primary_key: true)
-      # 交易所在区块
+      add(:end_block_number, :bigint, null: false)
+      # event_root
+      add(:event_root, :bytea, null: false)
+      # checkpoint总包含的事件数（另起线程统计l2_events中数据）
+      add(:event_counts, :integer, null: false)
+      # 交易所在L1区块
       add(:l1_block_number, :bigint, null: false)
       # checkpoint 批次在L1上的hash
       add(:l1_transaction_hash, :bytea, null: false)
-      # event_root
-      add(:event_root, :bytea, null: false)
-      # check point 周期
-      add(:epoch, :bigint, null: false)
-      # check point 周期 对应截止块高
-      add(:block_number, :bigint, null: false)
-      # checkpoint总交易数（根据事件中blockNumber推算出来）
-      add(:tx_number, :integer, null: false)
-      # checkpoint交易时间
+      # checkpoint交易所在L1交易时间
       add(:block_timestamp, :utc_datetime_usec, null: false)
-      # 0-waiting for state root 1-Ready for replay 2-Replayed（监听到事件应该批次处理成功了，所以目前应该只有2）
-      add(:status, :integer, null: false)
 
+      #record timestamp
       timestamps(null: false, type: :utc_datetime_usec)
     end
   end
