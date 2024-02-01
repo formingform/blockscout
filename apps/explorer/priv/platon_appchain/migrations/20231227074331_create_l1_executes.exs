b0caa88f4b0be4f7fac82e42b116a1893cfc51c0
formingform <formgingform.lyu@gmail.com>
2024-02-01 19:52:43 +0800

    add column: block_number

--- apps/explorer/priv/platon_appchain/migrations/20231227074331_create_l1_executes.exs
+++ apps/explorer/priv/platon_appchain/migrations/20231227074331_create_l1_executes.exs
@@ -11,6 +11,8 @@ defmodule Explorer.Repo.PlatonAppchain.Migrations.CreateL1TxExecutes do
       add(:amount, :numeric, precision: 100, null: true)
       # l1上交易hash
       add(:hash, :bytea, null: false)
+      # l1上交易所在区块
+      add(:block_number, :integer, null: false)
       # L1交易checkpoint批次提交hash（目前做关联checkpoints的l1_transaction_hash字段用，后续看是否删除）
       add(:state_batch_hash, :bytea, null: false)
       # 回放状态(业务状态，只有用户手工操作后，吐出事件才会更新) 0-未知 1-成功 2-失败 后续调合约确认哪值表示成功
