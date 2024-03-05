13ba3935095c41cbbb61087a9495ae5629d0c4fb
hudenian <hudenian@163.com>
2024-03-05 11:25:59 +0800

    blockchain模块相关接口文档开发

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v2/platon_appchain_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v2/platon_appchain_controller.ex
@@ -40,6 +40,34 @@ defmodule BlockScoutWeb.API.V2.PlatonAppchainController do
     |> render(:platon_appchain_items_count, %{count: count})
   end
 
+  @spec deposits_batches(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def deposits_batches(conn, params) do
+    {commitments, next_page} =
+      params
+      |> paging_options()
+      |> Keyword.put(:api?, true)
+      |> Query.deposits_batches()
+      |> split_list_by_page()
+
+    next_page_params = next_page_params(next_page, commitments, params)
+
+    conn
+    |> put_status(200)
+    |> render(:platon_appchain_deposits_batches, %{
+      commitments: commitments,
+      next_page_params: next_page_params
+    })
+  end
+
+  @spec deposits_batches_count(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def deposits_batches_count(conn, _params) do
+    count = Query.deposits_batches_count(api?: true)
+
+    conn
+    |> put_status(200)
+    |> render(:platon_appchain_items_count, %{count: count})
+  end
+
   @spec withdrawals(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def withdrawals(conn, params) do
     {withdrawals, next_page} =
@@ -62,6 +90,34 @@ defmodule BlockScoutWeb.API.V2.PlatonAppchainController do
   @spec withdrawals_count(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def withdrawals_count(conn, _params) do
     count = Query.withdrawals_count(api?: true)
+
+    conn
+    |> put_status(200)
+    |> render(:platon_appchain_items_count, %{count: count})
+  end
+
+  @spec withdrawals_batches(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def withdrawals_batches(conn, params) do
+    {withdrawals, next_page} =
+      params
+      |> paging_options()
+      |> Keyword.put(:api?, true)
+      |> Query.withdrawals_batches()
+      |> split_list_by_page()
+
+    next_page_params = next_page_params(next_page, withdrawals, params)
+
+    conn
+    |> put_status(200)
+    |> render(:platon_appchain_withdrawals_batches, %{
+      withdrawals: withdrawals,
+      next_page_params: next_page_params
+    })
+  end
+
+  @spec withdrawals_batches_count(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def withdrawals_batches_count(conn, _params) do
+    count = Query.withdrawals_batches_count(api?: true)
 
     conn
     |> put_status(200)
